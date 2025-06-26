f5345cb21c3f57c5993b97ed67b2e5b30fca2f10
christophe.bugnon <christophe@wemoms.fr>
2025-06-26 09:58:45 +0200

    [FEED CACHE] - save command does not request cache insertion on deletion error

--- UnsplashFeed/UnsplashFeedTests/FeedCacheUseCasesTests.swift
+++ UnsplashFeed/UnsplashFeedTests/FeedCacheUseCasesTests.swift
@@ -8,19 +8,46 @@
 import Testing
 
 class LocalFeedCache {
+    private let store: FeedStore
+
+    init(store: FeedStore) {
+        self.store = store
+    }
+
+    func save() {
+        store.deleteCachedFeed()
+    }
 }
 
 class FeedStore {
-    var messages: [String] = []
+    enum ReceivedMessage: Equatable {
+        case deleteCachedFeed
+    }
+
+    private(set) var messages: [ReceivedMessage] = []
+
+    func deleteCachedFeed() {
+        messages.append(.deleteCachedFeed)
+    }
 }
 
 class FeedCacheUseCasesTests {
 
     @Test
     func test_init_doesNotDeliversMessageUponCreation() {
-        let _ = LocalFeedCache()
         let store = FeedStore()
+        let _ = LocalFeedCache(store: store)
 
         #expect(store.messages == [])
+    }
+
+    @Test
+    func test_save_doesNotRequestFeedInsertionOnDeletionError() {
+        let store = FeedStore()
+        let sut = LocalFeedCache(store: store)
+
+        sut.save()
+
+        #expect(store.messages == [.deleteCachedFeed])
     }
 }
